9815ad70f0d1c9cfbdce3ae8be73f79d3ad0a671
leannejoylibertad-ctrl <leannejoy.libertad@wvsu.edu.ph>
2025-12-15 18:02:53 +0800

    ADD CODE
    ADD AND EDITED CODE FOR MANAGE MEMBERS CSS AND HTML

---
+++ managemembers.js
@@ -0,0 +1,9 @@
+<script>
+  const userProfile = document.getElementById("userProfile");
+  const dropdownMenu = document.getElementById("dropdownMenu");
+
+  userProfile.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("show");
+    userProfile.classList.toggle("open");
+  });
+</script>
